55fa7c6c389b4cf70be76755cfe5c5317eba9df6
Tomasz Drwięga <tomusdrw@gmail.com>
2017-11-30 10:07:09 +0100

    Re-format.

--- example/src/main.rs
+++ example/src/main.rs
@@ -55,21 +55,21 @@ fn anyone_should_be_able_to_register_a_badge() {
     evm.run(move |mut evm| {
         // Register new entry
         reg.register().transact(sol::address(10), sol::bytes32("test"),
-            evm
-            .with_value(unit::ether(2))
-            .with_sender(5.into())
-            .ensure_funds()
+        evm
+        .with_value(unit::ether(2))
+        .with_sender(5.into())
+        .ensure_funds()
         )?;
 
         // TODO [ToDr] The API here is crap, we need to work on sth better.
         // Check that the event has been fired.
         assert_eq!(
             evm.logs(badgereg::events::Registered::default().create_filter(
-                sol::bytes32("test"),
-                ethabi::Topic::Any,
-            )).len(),
-            1
-        );
+                    sol::bytes32("test"),
+                    ethabi::Topic::Any,
+                    )).len(),
+                    1
+                  );
 
         // TODO [ToDr] Perhaps `with_` should not be persistent?
         evm.with_value(0.into());
@@ -77,7 +77,7 @@ fn anyone_should_be_able_to_register_a_badge() {
         assert_eq!(
             reg.from_name().call(sol::bytes32("test"), &mut evm)?,
             (sol::raw::uint(0), sol::raw::address(10), sol::raw::address(5), )
-        );
+            );
 
         Ok(())
     })
--- solaris/src/trace.rs
+++ solaris/src/trace.rs
@@ -11,15 +11,15 @@ pub struct PrintingTracer {
     depth: usize,
     pc: usize,
     instruction: u8,
-	stack: Vec<U256>,
+    stack: Vec<U256>,
 }
 
 fn u256_as_str(v: &U256) -> String {
-	if v.is_zero() {
-		"0x0".into()
-	} else {
-		format!("{:x}", v)
-	}
+    if v.is_zero() {
+        "0x0".into()
+    } else {
+        format!("{:x}", v)
+    }
 }
 
 fn bytes_as_str(v: &Option<Bytes>) -> String {
@@ -30,10 +30,10 @@ fn bytes_as_str(v: &Option<Bytes>) -> String {
 }
 
 impl PrintingTracer {
-	fn stack(&self) -> String {
-		let items = self.stack.iter().map(u256_as_str).collect::<Vec<_>>();
-		format!("[{}]", items.join(","))
-	}
+    fn stack(&self) -> String {
+        let items = self.stack.iter().map(u256_as_str).collect::<Vec<_>>();
+        format!("[{}]", items.join(","))
+    }
 
     fn depth(&self) -> String {
         let mut s = String::new();
@@ -47,7 +47,7 @@ impl PrintingTracer {
 impl trace::Tracer for PrintingTracer {
     type Output = ();
 
-	fn prepare_trace_call(&self, params: &vm::ActionParams) -> Option<Call> {
+    fn prepare_trace_call(&self, params: &vm::ActionParams) -> Option<Call> {
         println!(
             "{d}CALL ({from} --{value:?}--> {to}), data: {data}",
             d = self.depth(),
@@ -55,90 +55,90 @@ impl trace::Tracer for PrintingTracer {
             value = params.value,
             to = params.address,
             data = bytes_as_str(&params.data),
-        );
+            );
         None
     }
 
-	fn prepare_trace_create(&self, params: &vm::ActionParams) -> Option<Create> {
+    fn prepare_trace_create(&self, params: &vm::ActionParams) -> Option<Create> {
         println!(
             "{d}CREATE ({from} --{value:?}--> NEW), data: {data}",
             d = self.depth(),
             from = params.sender,
             value = params.value,
             data = bytes_as_str(&params.data),
-        );
+            );
         None
     }
 
-	fn prepare_trace_output(&self) -> Option<Bytes> {
+    fn prepare_trace_output(&self) -> Option<Bytes> {
         None
     }
 
-	fn trace_call(
-		&mut self,
-		_call: Option<Call>,
-		_gas_used: U256,
-		output: Option<Bytes>,
-		_subs: Vec<Self::Output>,
-	) {
+    fn trace_call(
+        &mut self,
+        _call: Option<Call>,
+        _gas_used: U256,
+        output: Option<Bytes>,
+        _subs: Vec<Self::Output>,
+        ) {
         println!("{}<--Output: {} ", self.depth(), bytes_as_str(&output));
     }
 
-	/// Stores trace create info.
-	fn trace_create(
-		&mut self,
-		_create: Option<Create>,
-		_gas_used: U256,
-		_code: Option<Bytes>,
-		address: H160,
-		_subs: Vec<Self::Output>
-	) {
+    /// Stores trace create info.
+    fn trace_create(
+        &mut self,
+        _create: Option<Create>,
+        _gas_used: U256,
+        _code: Option<Bytes>,
+        address: H160,
+        _subs: Vec<Self::Output>
+        ) {
         println!("{}<--At: {}", self.depth(), address);
     }
 
-	fn trace_failed_call(&mut self, _call: Option<Call>, _subs: Vec<Self::Output>, error: trace::TraceError) {
+    fn trace_failed_call(&mut self, _call: Option<Call>, _subs: Vec<Self::Output>, error: trace::TraceError) {
         println!("{}CALL FAILED: {:?}", self.depth(), error);
     }
 
-	fn trace_failed_create(&mut self, _create: Option<Create>, _subs: Vec<Self::Output>, error: trace::TraceError) {
+    fn trace_failed_create(&mut self, _create: Option<Create>, _subs: Vec<Self::Output>, error: trace::TraceError) {
         println!("{}CREATE FAILED: {:?}", self.depth(), error);
     }
 
-	fn trace_suicide(&mut self, _address: H160, _balance: U256, _refund_address: H160) {}
+    fn trace_suicide(&mut self, _address: H160, _balance: U256, _refund_address: H160) {}
 
-	fn trace_reward(&mut self, _author: H160, _value: U256, _reward_type: trace::RewardType) {}
+    fn trace_reward(&mut self, _author: H160, _value: U256, _reward_type: trace::RewardType) {}
 
-	fn subtracer(&self) -> Self where Self: Sized {
-		let mut vm = PrintingTracer::default();
+    fn subtracer(&self) -> Self where Self: Sized {
+        let mut vm = PrintingTracer::default();
         vm.vm_enabled = self.vm_enabled;
-		vm.depth = self.depth + 1;
-		vm
+        vm.depth = self.depth + 1;
+        vm
     }
 
-	fn drain(self) -> Vec<Self::Output> {
+    fn drain(self) -> Vec<Self::Output> {
         vec![]
     }
 }
 
 impl trace::VMTracer for PrintingTracer {
-	type Output = ();
+    type Output = ();
 
-	fn trace_next_instruction(&mut self, pc: usize, instruction: u8) -> bool {
+    fn trace_next_instruction(&mut self, pc: usize, instruction: u8) -> bool {
         self.pc = pc;
         self.instruction = instruction;
-		true
-	}
+        true
+    }
 
-	fn trace_executed(&mut self, _gas_used: U256, stack_push: &[U256], _mem_diff: Option<(usize, &[u8])>, _store_diff: Option<(U256, U256)>) {
+    fn trace_executed(&mut self, _gas_used: U256, stack_push: &[U256], _mem_diff: Option<(usize, &[u8])>, _store_diff: Option<(U256, U256)>) {
         if !self.vm_enabled {
             return;
         }
 
-		let info = ::ethcore_evm::INSTRUCTIONS[self.instruction as usize];
+        let info = ::ethcore_evm::INSTRUCTIONS[self.instruction as usize];
 
-		let len = self.stack.len();
-		self.stack.truncate(if len > info.args { len - info.args } else { 0 });
-		self.stack.extend_from_slice(stack_push);
+        let len = self.stack.len();
+        self.stack.truncate(if len > info.args { len - info.args } else { 0 });
+        self.stack.extend_from_slice(stack_push);
 
         println!(
             "{}[{}] {}({:x}) stack_after: {}",
@@ -147,17 +147,17 @@ impl trace::VMTracer for PrintingTracer {
             info.name,
             self.instruction,
             self.stack()
-        );
-	}
+            );
+    }
 
-	fn prepare_subtrace(&self, _code: &[u8]) -> Self where Self: Sized {
-		let mut vm = PrintingTracer::default();
+    fn prepare_subtrace(&self, _code: &[u8]) -> Self where Self: Sized {
+        let mut vm = PrintingTracer::default();
         vm.vm_enabled = self.vm_enabled;
-		vm.depth = self.depth + 1;
-		vm
-	}
+        vm.depth = self.depth + 1;
+        vm
+    }
 
-	fn done_subtrace(&mut self, _sub: Self) {}
+    fn done_subtrace(&mut self, _sub: Self) {}
 
-	fn drain(self) -> Option<Self::Output> { None }
+    fn drain(self) -> Option<Self::Output> { None }
 }
