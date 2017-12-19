73e83fee8673c2594555316496e8e2867ca44ef9
Maximilian Krüger <kruemaxi@googlemail.com>
2017-12-19 12:58:14 +0100

    add test for every function in solaris::wei

--- solaris/src/wei.rs
+++ solaris/src/wei.rs
@@ -20,9 +20,23 @@ pub fn from_tether<T: Into<U256>>(value: T) -> U256 {
     value.into() * *TETHER
 }
 
+#[test]
+fn test_from_tether() {
+    assert_eq!(
+        U256::from_dec_str("2000000000000000000000000000000").unwrap(),
+        from_tether(2));
+}
+
 /// returns `value` converted from gether to wei
 pub fn from_gether<T: Into<U256>>(value: T) -> U256 {
     value.into() * *GETHER
+}
+
+#[test]
+fn test_from_gether() {
+    assert_eq!(
+        U256::from_dec_str("2000000000000000000000000000").unwrap(),
+        from_gether(2));
 }
 
 /// returns `value` converted from mether to wei
@@ -30,9 +44,23 @@ pub fn from_mether<T: Into<U256>>(value: T) -> U256 {
     value.into() * *METHER
 }
 
+#[test]
+fn test_from_mether() {
+    assert_eq!(
+        U256::from_dec_str("2000000000000000000000000").unwrap(),
+        from_mether(2));
+}
+
 /// returns `value` converted from kether/grand/einstein to wei
 pub fn from_kether<T: Into<U256>>(value: T) -> U256 {
     value.into() * *KETHER
+}
+
+#[test]
+fn test_from_kether() {
+    assert_eq!(
+        U256::from_dec_str("2000000000000000000000").unwrap(),
+        from_kether(2));
 }
 
 /// returns `value` converted from kether/grand/einstein to wei
@@ -40,9 +68,23 @@ pub fn from_grand<T: Into<U256>>(value: T) -> U256 {
     value.into() * *KETHER
 }
 
+#[test]
+fn test_from_grand() {
+    assert_eq!(
+        U256::from_dec_str("2000000000000000000000").unwrap(),
+        from_grand(2));
+}
+
 /// returns `value` converted from kether/grand/einstein to wei
 pub fn from_einstein<T: Into<U256>>(value: T) -> U256 {
     value.into() * *KETHER
+}
+
+#[test]
+fn test_from_einstein() {
+    assert_eq!(
+        U256::from_dec_str("2000000000000000000000").unwrap(),
+        from_einstein(2));
 }
 
 /// returns `value` converted from ether to wei
@@ -50,9 +92,23 @@ pub fn from_ether<T: Into<U256>>(value: T) -> U256 {
     value.into() * *ETHER
 }
 
+#[test]
+fn test_from_ether() {
+    assert_eq!(
+        U256::from_dec_str("2000000000000000000").unwrap(),
+        from_ether(2));
+}
+
 /// returns `value` converted from finney to wei
 pub fn from_finney<T: Into<U256>>(value: T) -> U256 {
     value.into() * *FINNEY
+}
+
+#[test]
+fn test_from_finney() {
+    assert_eq!(
+        U256::from_dec_str("2000000000000000").unwrap(),
+        from_finney(2));
 }
 
 /// returns `value` converted from szabo to wei
@@ -60,9 +116,23 @@ pub fn from_szabo<T: Into<U256>>(value: T) -> U256 {
     value.into() * *SZABO
 }
 
+#[test]
+fn test_from_szabo() {
+    assert_eq!(
+        U256::from_dec_str("2000000000000").unwrap(),
+        from_szabo(2));
+}
+
 /// returns `value` converted from gwei/shannon to wei
 pub fn from_gwei<T: Into<U256>>(value: T) -> U256 {
     value.into() * *GWEI
+}
+
+#[test]
+fn test_from_gwei() {
+    assert_eq!(
+        U256::from_dec_str("2000000000").unwrap(),
+        from_gwei(2));
 }
 
 /// returns `value` converted from gwei/shannon to wei
@@ -70,9 +140,23 @@ pub fn from_shannon<T: Into<U256>>(value: T) -> U256 {
     value.into() * *GWEI
 }
 
+#[test]
+fn test_from_shannon() {
+    assert_eq!(
+        U256::from_dec_str("2000000000").unwrap(),
+        from_shannon(2));
+}
+
 /// returns `value` converted from mwei/babbage to wei
 pub fn from_mwei<T: Into<U256>>(value: T) -> U256 {
     value.into() * *MWEI
+}
+
+#[test]
+fn test_from_mwei() {
+    assert_eq!(
+        U256::from_dec_str("2000000").unwrap(),
+        from_mwei(2));
 }
 
 /// returns `value` converted from mwei/babbage to wei
@@ -80,9 +164,23 @@ pub fn from_babbage<T: Into<U256>>(value: T) -> U256 {
     value.into() * *MWEI
 }
 
+#[test]
+fn test_from_babbage() {
+    assert_eq!(
+        U256::from_dec_str("2000000").unwrap(),
+        from_babbage(2));
+}
+
 /// returns `value` converted from kwei/ada to wei
 pub fn from_kwei<T: Into<U256>>(value: T) -> U256 {
     value.into() * *KWEI
+}
+
+#[test]
+fn test_from_kwei() {
+    assert_eq!(
+        U256::from_dec_str("2000").unwrap(),
+        from_kwei(2));
 }
 
 /// returns `value` converted from kwei/ada to wei
@@ -90,8 +188,22 @@ pub fn from_ada<T: Into<U256>>(value: T) -> U256 {
     value.into() * *KWEI
 }
 
+#[test]
+fn test_from_ada() {
+    assert_eq!(
+        U256::from_dec_str("2000").unwrap(),
+        from_ada(2));
+}
+
 /// returns `value` converted from wei to wei.
 /// unit function
 pub fn from_wei<T: Into<U256>>(value: T) -> U256 {
     value.into()
+}
+
+#[test]
+fn test_from_wei() {
+    assert_eq!(
+        U256::from(2),
+        from_wei(2));
 }
