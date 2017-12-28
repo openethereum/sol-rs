b4da1e2cc4f2e73ad059e5afc5d67b3a0f88ca3e
Maximilian Krüger <kruemaxi@googlemail.com>
2017-12-28 18:08:43 +0100

    remove todo

--- example/src/main.rs
+++ example/src/main.rs
@@ -75,8 +75,6 @@ fn anyone_should_be_able_to_register_a_badge() {
         )
         .unwrap();
 
-    // TODO [ToDr] The API here is crap, we need to work on sth better.
-    // Check that the event has been fired.
     assert_eq!(
         evm.logs(badgereg::events::Registered::default()).len(),
         1
